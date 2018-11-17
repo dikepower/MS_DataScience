47347e0dc3bda0a5244e8708f4ed03b377b7571e
HX <Hx@github.com>
2018-11-17 00:00:17 -0500

    Data607

---
+++ Data607_2018/Project2/Project2.R
@@ -0,0 +1,6 @@
+install.packages(c('openintro','OIdata','devtools','ggplot2','psych','reshape2',
+                   'knitr','markdown','shiny','R.rsp'))
+devtools::install_github("jbryer/DATA606")
+
+library('DATA606') 
+vignette(package='DATA606')
